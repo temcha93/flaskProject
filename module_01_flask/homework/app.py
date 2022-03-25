5c951eca2026257fd49afadd99dabcc740bf8ff5
temcha <artem_oparin@list.ru>
2022-03-25 11:27:15 +0600

    modul 1

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,34 +1,49 @@
 import datetime
 from flask import Flask
+from random import choice
+import random
 
 app = Flask(__name__)
 
 
 @app.route('/hello_world')
-def test_function():
-   pass
+def hello_function():
+
+    return 'Привет, мир!'
 
 
 @app.route('/cars')
-def test_function():
-   pass
+def cars_function():
+    return "Chevrolet, Renault, Ford, Lada"
 
 
 @app.route('/cats')
-def test_function():
-   pass
+def cats_function():
+    cats = ["корниш рекс", "русская голубая", "шотландская вислоухая", "мейн-кун", "манчкин"]
+    return choice(cats)
 
 
 @app.route('/get_time/now')
-def test_function():
-   pass
+def get_time_now_function():
+    current_time = datetime.datetime.now()
+    return "Точное время  " f"{current_time}"
 
 
 @app.route('/get_time/future')
-def test_function():
-   pass
+def get_time_future_function():
+    current_time_after_hour = datetime.datetime.now() + datetime.timedelta(hours=1)
+    return "Точное время  " f"{current_time_after_hour}"
 
 
 @app.route('/get_random_word')
-def test_function():
-   pass
+def word_function():
+    lines = random.choice(list(open('war_and_peace.txt', encoding='utf-8')))
+    return lines
+
+
+someGlobalVar = 0
+@app.route('/counter')
+def incrimentGlobalVar():
+    global someGlobalVar
+    someGlobalVar = someGlobalVar + 1
+    return "Количество раз  " f"{someGlobalVar}"
--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route('/test')
 def test_function():
-   return 'Это тестовая страничка, ответ сгенерирован в %s' % \
+    return 'Это тестовая страничка, ответ сгенерирован в %s' % \
                      datetime.datetime.now().utcnow()
